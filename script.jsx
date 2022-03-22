eff00c59529bfc6fc110e60d69b175979fc5a5e6
popovnv <nikitos.one@gmail.com>
2022-03-22 14:45:47 +0300

    Initial commit

---
+++ script.jsx
@@ -0,0 +1,24 @@
+class TimerWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    startTimer(timeLeft) {
+
+    }
+
+    render() {
+
+    }
+}
+
+ReactDOM.render(
+    <div>
+
+    </div>,
+    document.getElementById('timer-app')
+)
